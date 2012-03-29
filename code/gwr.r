cc6753ca4b20345962c7c171b155af2c3b00870a
Wesley Brooks <brooks.wesley@gmail.com>
2012-03-29 17:15:00 -0500

    gwr.r now calculates gwr estimates across the domain

--- code/gwr.r
+++ code/gwr.r
@@ -4,6 +4,11 @@ data(columbus)
 pov = read.csv("~/git/gwr/data/upMidWestpov_Iowa_cluster_names.csv", header=TRUE)
 heads = c('pindpov', 'logitindpov', 'pag', 'pex', 'pman', 'pserve', 'pfire', 'potprof', 'pwh', 'pblk', 'pind', 'phisp', 'metro', 'pfampov', 'logitfampov')
 years = c('60', '70', '80', '90', '00', '06')
+column.map = list(pindpov='proportion individuals in poverty', 
+    logitindpov='logit( proportion individuals in poverty )', pag='pag', pex='pex', pman='pman', 
+    pserve='pserve', potprof='potprof', pwh='proportion white', pblk='proportion black', pind='pind',
+    phisp='proportion hispanic', metro='metro', pfampov='proportion families in poverty',
+    logitfampov='logit( proportion families in poverty)')
 
 pov2 = list()
 
@@ -40,6 +45,59 @@ pov2 = data.frame(pov2)
 
 pov2 = within(pov2, year <- as.numeric(as.character(year)) + 1900)
 pov2 = within(pov2, year <- ifelse(year<1960, year+100, year))
+
+#Define the bisquare weight function:
+bisquare = function(x, z, bw) {
+    ifelse( r(x=x, z=c(z[1], z[2])) < bw, (1 - (r(x=x, z=c(z[1],z[2]))/ bw)**2)**2, 0)
+}
+
+bisquare = function(R, bw) {
+    ifelse( R < bw, (1 - (R/bw)**2)**2, 0)
+}
+
+#Define the distance function
+r = function(x, z) { 
+    sqrt((x[1]-z[1])**2 + (x[2]-z[2])**2)
+}
+
+#Fix an x.i and get the distance to all other x's:
+R = function(x.i, xy.mat) {
+    R = sapply(1:dim(xy.mat)[1], FUN=function(i) {r(x.i, xy.mat[i,])} )
+    return(R)
+}
+
+W = function(x.i, xy.mat, bw) {
+    distance = R(x.i, xy.mat)
+    W = bisquare(distance, bw)
+    return(W)
+}
+
+
+predictors = c('pag', 'pex', 'pman', 'pserve', 'pfire', 'potprof', 'pwh', 'pblk', 'phisp', 'metro')
+f = as.formula(paste("logitindpov ~ ", paste(predictors, collapse="+"), sep=""))
+
+for (col in predictors) {
+    assign(col, vector())
+}
+
+df = pov2[pov2$year==2006,]
+
+for(i in 1:dim(df)[1]) {
+    w = W(df[i, c('x', 'y')], df[,c('x','y')], bw=1)
+
+    model = lm(f, data=df, weights=w)
+    
+    w.eig <- eigen(diag(w))
+    w.sqrt <- w.eig$vectors %*% diag(sqrt(w.eig$values)) %*% solve(w.eig$vectors)
+    w.lasso[[i]] = lars(x=w.sqrt %*% as.matrix(df[,predictors]), y=as.matrix(df$logitindpov))
+
+    for (col in predictors) {
+        coef = get(col)
+        assign(col, c(coef, model$coef[[col]]))
+    }
+    
+    print(i)
+}
 
 bw = gwr.sel(income~1, data=columbus, coords=cbind(x,y), adapt=FALSE, gweight=gwr.bisquare)
 gwr.model1 = gwr(income~1, data=columbus, coords=cbind(x,y), bandwidth=bw, gweight=gwr.bisquare, hatmatrix=TRUE)
