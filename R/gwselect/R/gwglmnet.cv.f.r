350abd5654e924ec114b755b23d33c9e02c398c2
Wesley Brooks <brooks.wesley@gmail.com>
2013-09-11 17:23:57 -0500

    comparing branches

--- R/gwselect/R/gwglmnet.cv.f.r
+++ R/gwselect/R/gwglmnet.cv.f.r
@@ -6,10 +6,10 @@ gwglmnet.cv.f = function(formula, data, weights, indx, family, bw, coords, gweig
     if (!AICc) { loss = sum(sapply(gwglmnet.model[['model']][['models']], function(x) {min(x[['loss.local']])})) }
     else {
         trH = sum(sapply(gwglmnet.model[['model']][['models']], function(x) {x[['loss.local']]})) 
-        #print(log(mean(sapply(gwlars.model[['model']][['models']], function(x) {x[['sigma2']]}))))
-        #print(nrow(data))
-        #print(trH)
-        #print((2*(trH+1))/(nrow(data)-trH-2))
+        print(trH)
+        print((2*(trH+1))/(nrow(data)-trH-2))
+        print(log(mean(sapply(gwglmnet.model[['model']][['models']], function(x) {x[['ssr.local']]}))))
+        print(log(mean(sapply(gwglmnet.model[['model']][['models']], function(x) {x[['ssr.local']]}))) + 1 + (2*(trH+1))/(nrow(data)-trH-2) + log(2*pi))
         
         #Local sigma^2:
         #loss = nrow(data) * (mean(sapply(gwglmnet.model[['model']][['models']], function(x) {log(x[['sigma2']])})) + 1 + (2*(trH+1))/(nrow(data)-trH-2) + log(2*pi))
