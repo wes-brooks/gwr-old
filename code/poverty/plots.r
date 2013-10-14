53784741fb2d41654eece243558506b5d80579fb
Wesley Brooks <brooks.wesley@gmail.com>
2013-10-14 15:55:53 -0500

    more changes to the poverty/plots.r script

--- code/poverty/plots.r
+++ code/poverty/plots.r
@@ -16,22 +16,22 @@ for (yr in years) {
     #Correct the locations of some small counties (only affect plotting)
 	cluster_id = which(df$COUNTY=="WI_CLUSTER")
 	n.counties = nrow(df)
+    year = as.character(yr)
 
     #for (select in c("lasso", "enet")) {
     for (select in c("enet")) {
-        model[[select]][[yr]][['model']][['models']][[n.counties+1]] = model[[select]][[yr]][['model']][['models']][[cluster_id]]
-        model[[select]][[yr]][['coords']][df$STATE=='Wisconsin' & df$COUNTY=='PEPIN',] = c(-92.1048, 44.5823)
-        model[[select]][[yr]][['coords']] = rbind(model[[select]][[yr]][['coords']], c(-88.7285, 44.9291))
+        model[[select]][[year]][['model']][['models']][[n.counties+1]] = model[[select]][[year]][['model']][['models']][[cluster_id]]
+        model[[select]][[year]][['coords']][df$STATE=='Wisconsin' & df$COUNTY=='PEPIN',] = c(-92.1048, 44.5823)
+        model[[select]][[year]][['coords']] = rbind(model[[select]][[year]][['coords']], c(-88.7285, 44.9291))
     
         plots[[select]][[year]] = list()
         for (v in predictors) {
-            plots[[select]][[yr]][[v]] = plot.gwselect(model[[select]][[yr]], part='coef.unshrunk', var=v, polygons=county, title=v, col.bg='gray85') + theme(plot.margin=unit(c(0,0,0,1), "cm")) + scale_x_continuous('') + scale_y_continuous('')
+            plots[[select]][[year]][[v]] = plot.gwselect(model[[select]][[year]], part='coef.unshrunk', var=v, polygons=county, title=v, col.bg='gray85') + theme(plot.margin=unit(c(0,0,0,1), "cm")) + scale_x_continuous('') + scale_y_continuous('')
         }
 
         pp = plots[[select]][[year]]
         #dev.new()
         pdf(paste('figures/poverty/', yr, '-', select, '-linear-coefficients-unshrunk.pdf', sep=''), width=8, height=16)
-        #grid.arrange(pp[['pag']], pp[['pex']], pp[['pman']], pp[['potprof']], pp[['pfire']], pp[['pserve']], pp[['pwh']], pp[['pblk']], pp[['phisp']], pp[['metro']], ncol=2)
         grid.arrange(pp[['pag']], pp[['pex']], pp[['pman']], pp[['potprof']], pp[['pfire']], pp[['pserve']], ncol=2)
         dev.off()
     }
