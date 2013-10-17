392d54e9801d8b843c1889f39793c5121a78d9fe
Wesley Brooks <brooks.wesley@gmail.com>
2013-10-17 12:25:02 -0500

    added gwen-gwr-scatter.r to plotting makefile

--- code/poverty/gwen-gwr-scatter.r
+++ code/poverty/gwen-gwr-scatter.r
@@ -17,29 +17,36 @@ title.map = list(pindpov='Proportion of individuals in poverty',
 
 
 pp = list()
+years = c(1970)
+predictors = c('pag', 'pex', 'pman', 'pserve', 'pfire', 'potprof')
 
-for (k in 1:length(predictors)) {
-    v = predictors[k]
-    cc = sapply(model[['GWEN']][['1970']][['model']][['models']], function(x) {x[['coef.unshrunk']][k+1]})
-    cc = cbind(cc, sapply(model[['GWR']][['1970']][['model']][['models']], function(x) {x[['coef']][k+1]}))
-    cc = as.data.frame(cc)
-    colnames(cc) = c('GWEN', 'GWR')
+for (yr in years) {
+    yr = as.character(yr)
 
-    pp[[k]] = ggplot(cc) +
-        aes(x=GWR, y=GWEN) +
-        geom_point(size=1) + #position=position_jitter(height=0.01), 
-        labs(title=title.map[[v]]) +
-        theme(title=element_text(vjust=1)) +
-        theme(plot.margin=unit(c(0,0,0,0),'cm'), legend.margin=unit(0,'cm')) +
-        theme_bw()
+    for (k in 1:length(predictors)) {
+        v = predictors[k]
+        cc = sapply(model[['GWEN']][[yr]][['model']][['models']], function(x) {x[['coef.unshrunk']][k+1]})
+        cc = cbind(cc, sapply(model[['GWR']][[yr]][['model']][['models']], function(x) {x[['coef']][k+1]}))
+        cc = as.data.frame(cc)
+        colnames(cc) = c('GWEN', 'GWR')
+
+        pp[[k]] = ggplot(cc) +
+            aes(x=GWR, y=GWEN) +
+            geom_point(size=1) + #position=position_jitter(height=0.01), 
+            geom_abline() +
+            labs(title=title.map[[v]]) +
+            theme(title=element_text(vjust=1)) +
+            theme(plot.margin=unit(c(0,0,0,0),'cm'), legend.margin=unit(0,'cm')) +
+            theme_bw()
+    }
+
+    #For the prelim slides
+    pdf(paste('~/git/gwr/figures/practice-talk/', yr, '-GWEN-GWR-comparison.pdf', sep=''), width=11, height=6)
+    brooks::multiplot(plotlist=pp, cols=3)
+    dev.off()
+
+    #For the prelim paper
+    pdf(paste('~/git/gwr/figures/poverty/', yr, '-GWEN-GWR-comparison.pdf', sep=''), width=8, height=11)
+    brooks::multiplot(plotlist=pp, cols=2)
+    dev.off()
 }
-
-#For the prelim slides
-pdf(paste('~/git/gwr/figures/practice-talk/', yr, '-GWEN-GWR-comparison.pdf', sep=''), width=11, height=6)
-brooks::multiplot(plotlist=pp, cols=3)
-dev.off()
-
-#For the prelim paper
-pdf(paste('~/git/gwr/figures/poverty/', yr, '-GWEN-GWR-comparison.pdf', sep=''), width=8, height=11)
-brooks::multiplot(plotlist=pp, cols=2)
-dev.off()
